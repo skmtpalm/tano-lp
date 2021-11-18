a627eb2389e5f7f59d3dba976d6341281fa7274f
skmtpalm <hachihachi.8og@gmail.com>
2021-11-18 11:15:59 +0900

    add clsx

--- src/components/DealersInfo/DealersInfo.js
+++ src/components/DealersInfo/DealersInfo.js
@@ -8,16 +8,16 @@ const DEALERS = [
     url: "https://kocuu.com/",
   },
   {
-    name: "Un jour MARCHE",
-    address: "静岡市葵区茶町1-27-2",
-    tel: "054-255-6007",
-    url: "https://kocuu.com/",
+    name: "SAKE・キリンヤ",
+    address: "藤枝市大洲1-9-13",
+    tel: "054-635-8210",
+    url: "https://sake-kirinya.jp/",
   },
   {
-    name: "Un jour MARCHE",
-    address: "静岡市葵区茶町1-27-2",
-    tel: "054-255-6007",
-    url: "https://kocuu.com/",
+    name: "リカーズ グリーン",
+    address: "焼津市中新田251-3",
+    tel: "054-624-3210",
+    url: "https://green.ocnk.net/",
   },
 ];
 
---
+++ src/components/NavButton/NavButton.js
@@ -0,0 +1,10 @@
+const NavButton = () => {
+  return (
+    <div>
+      <span>-</span>
+      <span>-</span>
+    </div>
+  );
+};
+
+export default NavButton;
