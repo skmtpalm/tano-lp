ad1d782345040df663ae6e074a21deed94e17b12
skmtpalm <hachihachi.8og@gmail.com>
2021-11-18 16:49:29 +0900

    fix navigation

--- src/components/Navigation/AnchorLinkItem.js
+++ src/components/Navigation/AnchorLinkItem.js
@@ -1,10 +1,24 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { useInView } from "react-intersection-observer";
+import clsx from "clsx";
+import s from "./Navigation.module.css";
 
-const AnchorLinkItem = ({ label, to, setNavOpen }) => {
+const AnchorLinkItem = ({ label, labelEn, to, setNavOpen, delay }) => {
+  const [ref, inView] = useInView({
+    delay: Number(delay),
+  });
+
   return (
-    <li>
+    <li
+      ref={ref}
+      className={clsx(s.item, {
+        [s.show]: inView,
+      })}
+      style={{ transitionDelay: `0.${delay}s` }}
+    >
       <AnchorLink onClick={() => setNavOpen(false)} href={`#${to}`}>
-        {label}
+        <span className={clsx(s.labelEn)}>{labelEn}</span>
+        <span className={clsx(s.label)}>{label}</span>
       </AnchorLink>
     </li>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,39 +1,46 @@
-import { useState } from "react";
 import AnchorLinkItem from "./AnchorLinkItem";
 import clsx from "clsx";
 import s from "./Navigation.module.css";
 
 const LINKS = [
   {
-    label: "TOP",
+    label: "トップ",
+    labelEn: "top",
     to: "top",
   },
   {
     label: "こだわり",
+    labelEn: "feature",
     to: "feature",
   },
   {
     label: "購入方法",
+    labelEn: "How to buy",
     to: "howtobuy",
   },
   {
     label: "生産者紹介",
+    labelEn: "Producers",
     to: "producers",
   },
   {
     label: "地域とともに歩む",
+    labelEn: "pray",
     to: "outro",
   },
   {
     label: "ななめぐりプロジェクト",
+    labelEn: "nanameguri project",
     to: "nanameguriproject",
   },
   {
     label: "特約店",
+    labelEn: "dealers",
     to: "dealers",
   },
   {
     label: "お問い合わせ",
+    labelEn: "contact",
     to: "contact",
   },
 ];
@@ -45,12 +52,14 @@ const Navigation = ({ isOpen, setNavOpen }) => {
         [s.open]: isOpen,
       })}
     >
-      <ul>
-        {LINKS.map((link) => (
+      <ul className="px-6">
+        {LINKS.map((link, index) => (
           <AnchorLinkItem
+            delay={index}
             setNavOpen={setNavOpen}
             key={link.to}
             label={link.label}
+            labelEn={link.labelEn}
             to={link.to}
           />
         ))}
