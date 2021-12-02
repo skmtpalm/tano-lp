d17d0e38c4e0cb952e9bde6f9dc4a8059b958604
skmtpalm <hachihachi.8og@gmail.com>
2021-12-02 09:01:35 +0900

    Add ga tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,27 @@
 import "../../styles/globals.css";
 import Layout from "../components/Layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id="ga-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
