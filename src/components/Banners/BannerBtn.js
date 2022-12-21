4542470d478ea1b37a0ce98a35029a7f7a047cdd
okysk <hachihachi.8og@gmail.com>
2022-12-21 15:31:43 +0900

    ECサイトへのリンクを修正

--- src/components/Banners/BannerBtn.js
+++ src/components/Banners/BannerBtn.js
@@ -3,7 +3,7 @@ import s from "./BannerBtn.module.css"
 
 const BannerBtn = ({ title, text }) => {
   return (
-    <a href="https://sonodanouen.official.ec/items/55529795" className={clsx(s.wrapper, "mb-4")}>
+    <a href="https://sonodanouen.official.ec/" className={clsx(s.wrapper, "mb-4")}>
       <span className={clsx("text-xl sm:text-3xl font-serif mb-2")}>
         {title}
       </span>
